55573ffd3be7748d183a2aba1638f2fed6cf6916
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-08-10 17:31:36 -0500

    estilo de resultados de busqueda

--- themes/unitheme/search.php
+++ themes/unitheme/search.php
@@ -1,3 +1,4 @@
+
 <?php get_header(); ?>
 <div class="search-results-title">
 	<div class='container nz-clearfix'>
@@ -12,14 +13,14 @@
 	<div class='content nz-clearfix'>
 
 		<div class="search-form">
-			<?php 
+			<?php
 				$args = array(
 					'before_widget'=>'',
 					'after_widget' =>'',
 					'before_title' =>'',
 					'after_title'  =>''
 				);
-				the_widget( 'WP_Widget_Search', $args ); 
+				the_widget( 'WP_Widget_Search', $args );
 			?>
 		</div>
 
@@ -48,26 +49,35 @@
 								</a>
 							</h3>
 						<?php endif; ?>
+						<div class="[ columns-search ]">
+							<div class="[ col-1_2 ]">
+								<img class="" src="http://localhost:8888/fundacion-donde/wp-content/uploads/2016/08/2.jpg">
+							</div><!--col-1-->
+							<div class="[ col-2_2 ]">
+								<div class="post-meta nz-clearfix [ hidden ]">
+									<div class="post-author"><span class="icon-user2"></span><?php echo __("Posted by", TEMPNAME); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>" title="<?php echo __('View all posts by', TEMPNAME); ?> <?php the_author(); ?>"><?php the_author(); ?></a></div>
 
-						<div class="post-meta nz-clearfix">
-							<div class="post-author"><span class="icon-user2"></span><?php echo __("Posted by", TEMPNAME); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>" title="<?php echo __('View all posts by', TEMPNAME); ?> <?php the_author(); ?>"><?php the_author(); ?></a></div>
-							
-							<?php if ($post_type == "post"): ?>
-								<div class="post-category nz-clearfix"><span class="icon-list3"></span><?php the_category("<span class='comma'>, </span>"); ?></div>
-							<?php elseif($post_type == "portfolio"): ?>
-								<div class="post-category nz-clearfix"><span class="icon-list3"></span><?php echo get_the_term_list( $post->ID, 'portfolio-category', '', ', ', '' ); ?></div>
-							<?php endif ?>
+									<?php if ($post_type == "post"): ?>
+										<div class="post-category nz-clearfix"><span class="icon-list3"></span><?php the_category("<span class='comma'>, </span>"); ?></div>
+									<?php elseif($post_type == "portfolio"): ?>
+										<div class="post-category nz-clearfix"><span class="icon-list3"></span><?php echo get_the_term_list( $post->ID, 'portfolio-category', '', ', ', '' ); ?></div>
+									<?php endif ?>
 
-						</div>
-						<?php if ( '' != get_the_content() ): ?>
-						<div class="post-content nz-clearfix">
-							<?php the_excerpt(); ?>
-						</div>
-						<?php endif; ?>
-						<a href="<?php the_permalink(); ?>" class="button search-button small"><?php echo __("Read more", TEMPNAME); ?></a>
+								</div>
+								<?php if ( '' != get_the_content() ): ?>
+								<div class="post-content nz-clearfix [ margin-bottom--small ]">
+									<?php the_excerpt(); ?>
+								</div>
+								<?php endif; ?>
+								<p>$100.00</p>
+
+								<?php echo $product->get_categories( ', ', '<p class="posted_in">' . _n( 'Category:', 'Categories:', $cat_count, 'woocommerce' ) . ' ', '</p>' ); ?>
+								<a href="<?php the_permalink(); ?>" class="button search-button small"><?php echo __("Read more", TEMPNAME); ?></a>
+							</div> <!--end col-2-->
+						</div><!--end col2-set-->
 					</div>
 				</article>
-				
+
 			<?php endwhile; ?>
 
 			<?php ninzio_post_nav_num(); ?>
@@ -86,6 +96,6 @@
 		<?php endif; ?>
 
 	</div>
-	
+
 </div>
 <?php get_footer(); ?>
