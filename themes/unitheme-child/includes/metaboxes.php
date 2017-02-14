d45879e6077c59348c43dfcba4416da054709577
Jonas Graterol <jonas@pcuervo.com>
2017-02-14 08:51:07 -0600

    Numero de sucursal meta

--- themes/unitheme-child/includes/metaboxes.php
+++ themes/unitheme-child/includes/metaboxes.php
@@ -1,17 +1,26 @@
 <?php
-	//ACTIVIDADES METABOX
+	//SUCURSALES METABOX
 	function show_metabox_sucursal($post){
 		
-
+		$numero_sucursal = get_post_meta($post->ID, 'numero_sucursal', true);
 		$email_responsable_principal = get_post_meta($post->ID, 'email_responsable_principal', true);
 		$email_responsable_secundario = get_post_meta($post->ID, 'email_responsable_secundario', true);
 		$email_responsable_terciario = get_post_meta($post->ID, 'email_responsable_terciario', true);
 		$ubicacion = get_post_meta($post->ID, 'ubicacion', true);
 		
+		wp_nonce_field(__FILE__, '_numero_sucursal_nonce');
 		wp_nonce_field(__FILE__, '_email_responsable_principal_nonce');
 		wp_nonce_field(__FILE__, '_email_responsable_secundario_nonce');
 		wp_nonce_field(__FILE__, '_email_responsable_terciario_nonce');
 		wp_nonce_field(__FILE__, '_ubicacion_nonce');
+		echo '<div class="row">';
+			echo '<div class="md-12" style="width:100%; float:left;">';
+				echo '<strong>Numero de Sucursal: </strong>';
+				echo '<input type="number" class="" name="numero_sucursal" id="numero_sucursal" value="'.$numero_sucursal.'"  />';
+			echo '</div>';
+			echo '<br>';
+		echo '<br>';
+		echo '</div>';
 		echo '<div class="row">';
 			echo '<div class="md-6" style="width:33%; float:left;">';
 				echo '<strong>Email responsable principal: </strong>';
@@ -28,6 +37,8 @@
 		echo '</div>';
 		echo '<br>';
 		echo '<br>';
+		echo '<br>';
+		echo '<br>';
 		
 		
 		
@@ -37,7 +48,9 @@
 	* Save the metaboxes for post type "Actividad"
 	* */
 	add_action( 'save_post', function ( $post_id ){
-
+		if ( isset($_POST['numero_sucursal']) and check_admin_referer(__FILE__, '_numero_sucursal_nonce') ){
+			update_post_meta($post_id, 'numero_sucursal', $_POST['numero_sucursal']);
+		}
 		if ( isset($_POST['email_responsable_principal']) and check_admin_referer(__FILE__, '_email_responsable_principal_nonce') ){
 			update_post_meta($post_id, 'email_responsable_principal', $_POST['email_responsable_principal']);
 		}
@@ -52,7 +65,7 @@
 
 	add_action('add_meta_boxes', function(){
 		global $post;
-		add_meta_box( 'meta-box-sucursal', 'Emails Responsables', 'show_metabox_sucursal', 'sucursal');
+		add_meta_box( 'meta-box-sucursal', 'Datos adicionales', 'show_metabox_sucursal', 'sucursal');
 		
 	});
 
