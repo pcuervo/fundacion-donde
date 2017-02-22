fc123ed33a5340d0e04733e315cdcef79930a91d
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2017-02-22 12:19:21 -0600

    nosotros sin visual composer

--- themes/unitheme-child/inc/pages.php
+++ themes/unitheme-child/inc/pages.php
@@ -29,4 +29,16 @@ add_action('init', function(){
 		wp_insert_post( $page, true );
 	}
 
+	// Nosotros
+	if( ! get_page_by_path('nosotros') ){
+		$page = array(
+			'post_author' => 1,
+			'post_status' => 'publish',
+			'post_title'  => 'Nosotros',
+			'post_name'   => 'nosotros',
+			'post_type'   => 'page'
+		);
+		wp_insert_post( $page, true );
+	}
+
 });
---
+++ themes/unitheme-child/page-nosotros.php
@@ -0,0 +1,127 @@
+<?php get_header( ); ?>
+<section class="[ container ]">
+	<div class="nz-section horizontal animate-false full-width-true [ pading-top-bottom ]" data-animation-speed="35000" data-parallax="false">
+		<div class="nz-row">
+			<div class="col col12  col-animate-true active" data-align="left" data-effect="fade-bottom">
+				<div class="col-inner">
+					<h2 class="[ color-primary ]">Quiénes Somos</h2>
+					<div class="nz-column-text nz-clearfix">La Fundación Rafael Dondé MR es una Institución de Asistencia Privada (I.A.P.) sin fines de lucro, creada en 1905 gracias al sueño de su fundador el Lic. Rafael Dondé Preciat, quien hizo suyo el compromiso de ayudar a los más necesitados a través de la educación.</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="info-nosotros" class="nz-section horizontal animate-false full-width-false [ pading-top-bottom ] " data-animation-speed="35000" data-parallax="false">
+		<div class="nz-row">
+			<div class="col col6  col-animate-true active" data-align="left" data-effect="fade-left">
+				<div class="col-inner">
+					<div class="nz-content-box nz-clearfix v1 fade" data-columns="1">
+						<div id="nz-box-1" class="border-active  nz-box active">
+							<div class="box-icon-wrap">
+								<div class="box-icon icon-badge"></div>
+							</div>
+							<div class="box-data">
+								<h3 class="[ text-center ]">Misión</h3>
+								<p>Encaminarnos de forma expansiva en el desarrollo del potencial humano de todos los integrantes de la familia Fundación Dondé&nbsp;MR; en darle a los niños herramientas para que se reconozcan y realicen como seres valiosos, únicos y con facultades extraordinarias; y en ofrecer los mejores créditos y soluciones financieras con apoyos y acompañamientos que ayuden a mejorar la vida de la mayoría de los mexicanos.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col col6  col-animate-true active" data-align="left" data-effect="fade-right">
+				<div class="col-inner">
+					<div class="nz-content-box nz-clearfix v1 fade" data-columns="1">
+						<div id="nz-box-2" class="border-active  nz-box active">
+							<div class="box-icon-wrap">
+								<div class="box-icon icon-badge"></div>
+							</div>
+							<div class="box-data">
+								<h3 class="[ text-center ]">Visión</h3>
+								<p>Somos una sabia y poderosa comunión de seres valiosos, con un origen y un destino, entregados al servicio de la humanidad; que sentimos y expresamos amor por servir; y que estamos comprometidos con la libertad, con el amor y con nuestro desarrollo como seres humanos.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="nz-section horizontal animate-false full-width-false " data-animation-speed="35000" data-parallax="false">
+		<div class="nz-row">
+			<div class="col col12  col-animate-true active" data-align="left" data-effect="fade-bottom">
+				<div class="col-inner">
+					<h2 class="[ border-bottom--secondary ][ color-primary ]">Valores</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="nz-section horizontal animate-false full-width-false [ pading-top-bottom ]" data-animation-speed="35000" data-parallax="false">
+		<div class="nz-row">
+			<div class="col col4  col-animate-true active" data-align="left" data-effect="fade-right">
+				<div class="col-inner">
+					<ul class="nz-i-list circle">
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Agradecimiento</div>
+						</li>
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Autonomía y Libertad</div>
+						</li>
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Amor, Alegría y Gozo</div>
+						</li>
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Creatividad e Innovación</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col col4  col-animate-true active" data-align="left" data-effect="fade-right">
+				<div class="col-inner">
+					<ul class="nz-i-list circle">
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Disciplina y Perseverancia</div>
+						</li>
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Valentía y empuje</div>
+						</li>
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Trabajo en equipo</div>
+						</li>
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Empatía y respeto a la persona humana</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col col4  col-animate-true active" data-align="left" data-effect="fade-right">
+				<div class="col-inner">
+					<ul class="nz-i-list circle">
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>El trabajo es divinizante</div>
+						</li>
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Responsabilidad</div>
+						</li>
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Honestidad</div>
+						</li>
+						<li>
+							<div><span class="icon icon-checkmark [ color-primary ]"></span></div>
+							<div>Capacidad de adaptación al cambio</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php get_footer( ); ?>
