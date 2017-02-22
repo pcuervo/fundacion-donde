279951de01f2012a521fc6d6716993df8efd1f59
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2017-02-22 13:20:21 -0600

    home sin visual composer

--- themes/unitheme-child/page-inicio.php
+++ themes/unitheme-child/page-inicio.php
@@ -1,3 +1,149 @@
 <?php get_header( ); ?>
+<div id="ninzio-slider" data-autoheight="true" data-arrow="false" data-parallax="false" data-fixed="false" data-transition="side-swing" data-layout="wide" data-height="650" data-mobile="false" data-autoplaydelay="9000" data-autoplay="true" data-bullets="false">
+	<div class="slider-loader hidden" style="display: none;">&nbsp;</div>
+	<div id="slider-arrow" data-target="#nz-content" class="i-separator animate nz-clearfix vis"><i class="icon-arrow-down2"></i></div>
+	<ul class="ninzio-slides">
+		<li class="post-61 ninzio-slider type-ninzio-slider status-publish hentry navOutNext" data-video="false" id="post-61">
+			<div class="slider-canvas container">
+				<div id="ninzio-layer-61-1" class="ninzio-layer none" data-direction="none" data-posx="0" data-posy="0">
+					<div class="layer-wrap" style="opacity: 1;">
+						<p><span class="[ color-light font-weight-bold font-size--xlarge ]">Anillos de compromiso</span></p>
+						<p><span class="[ color-light font-size--xlarge ]">al mejor precio</span></p>
+						<p><span class="[ color-light font-size--xlarge ]">y a un click de distancia</span></p>
+						<p>
+							<a class="button button-3d green medium rounded animate-false anim-type-ghost" href="#" target="_self"><span class="txt">VER MODELOS</span></a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="post-194 ninzio-slider type-ninzio-slider status-publish hentry navInNext active" data-video="false" id="post-194">
+			<div class="slider-canvas container">
+				<div id="ninzio-layer-194-1" class="ninzio-layer none" data-direction="none" data-posx="0" data-posy="0">
+					<div class="layer-wrap" style="opacity: 1;">
+						<p><span class="[ color-light font-weight-bold font-size--xlarge ]">Lorem ipsum dolor sit</span></p>
+						<p><span class="[ color-light font-size--xlarge ]"">lorem ipsum dolor</span></p>
+						<p><span class="[ color-light font-size--xlarge ]"">dolore magna</span></p>
+						<a class="button button-3d green medium rounded animate-false anim-type-ghost" href="#" target="_self"><span class="txt">VER MODELOS</span></a>
+					</div>
+				</div>
+			</div>
+		</li>
+	</ul>
+	<span data-direction="prev" class="controls prev slider-nav icon-arrow-left8"></span><span data-direction="next" class="controls next slider-nav icon-arrow-right8"></span>
+</div>
 
+<div class="container page-full-width">
+	<!-- content start -->
+	<div id="nz-content" class="content nz-clearfix padding-true">
+		<!-- post start -->
+		<div id="post-39" class="post-39 page type-page status-publish hentry">
+			<section class="page-content nz-clearfix">
+				<div class="[ container ]">
+					<?php echo do_shortcode('[nz_rposts version="masonry" cat="home" columns="3"]'); ?>
+				</div>
+				<div class="nz-section horizontal animate-false full-width-true " data-animation-speed="35000" data-parallax="false">
+					<div class="container">
+						<h2 class="[ color-primary ]">Novedades</h2>
+						<?php echo do_shortcode('[product_category per_page="4" columns="4" orderby="date" order="" category="novedades"]'); ?>
+					</div>
+				</div>
+				<div class="nz-section horizontal animate-false full-width-true " data-animation-speed="35000" data-parallax="false">
+					<div class="container">
+						<h2 class="[ color-primary ]">Artículos destacados</h2>
+						<?php echo do_shortcode('[product_category per_page="4" columns="4" orderby="date" order="" category="articulos-destacados"]'); ?>
+					</div>
+				</div>
+
+				<div class="nz-section horizontal animate-false full-width-false [ bg-testimonials ]" data-img-width="1920" data-img-height="666" data-animation-speed="35000" data-parallax="false">
+					<div class="nz-row">
+						<div class="col col12  col-animate-false active" data-align="left" data-effect="fade-left">
+							<div class="col-inner">
+							<?php echo do_shortcode('
+								[nz_testimonials color="#ffffff"][nz_testimonial img="133" name="Autor Apellido" title="Estudiante"]
+									Los mejores productos al mejor precio y en perfectas condiciones, no lo podría recomendar más. ¡Excelente experiencia!
+								[/nz_testimonial][nz_testimonial img="135" name="Autor Apellido" title="Estudiante"]
+									Los mejores productos al mejor precio y en perfectas condiciones, no lo podría recomendar más. ¡Excelente experiencia!
+								[/nz_testimonial][nz_testimonial img="136" name="Autor Apellido" title="Estudiante"]
+									Los mejores productos al mejor precio y en perfectas condiciones, no lo podría recomendar más. ¡Excelente experiencia!
+								[/nz_testimonial][nz_testimonial img="134" name="Autor Lorem" title="Profesional"]
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat.
+								[/nz_testimonial][/nz_testimonials]
+							'); ?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="nz-section horizontal animate-false full-width-true " data-animation-speed="35000" data-parallax="false" style="padding-top:50px;padding-bottom:10px;">
+					<div class="container">
+						<div class="nz-row">
+							<div class="col col4  col-animate-false active" data-align="left" data-effect="fade-left">
+								<div class="col-inner">
+									<div class="nz-content-box nz-clearfix v1 fade" data-columns="1">
+										<div id="nz-box-1" class="border-active  nz-box active">
+											<div class="box-icon-wrap">
+												<div class="box-icon icon-badge"></div>
+											</div>
+											<div class="box-data">
+												<h3 class="text-center color-primary">Calidad garantizada</h3>
+												<p class="p1 text-center">Todos nuestros artículos están revisados y avalados con los más altos estándares de calidad</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col col4  col-animate-false active" data-align="left" data-effect="fade-left">
+								<div class="col-inner">
+									<div class="nz-content-box nz-clearfix v1 fade" data-columns="1">
+										<div id="nz-box-2" class="border-active  nz-box active">
+											<div class="box-icon-wrap">
+												<div class="box-icon icon-envio"></div>
+											</div>
+											<div class="box-data">
+												<h3 class="text-center color-primary">Envío express</h3>
+												<p class="p1 text-center">Entrega en cualquier punto de la república en máximo 5 días hábiles</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col col4  col-animate-false active" data-align="left" data-effect="fade-left">
+								<div class="col-inner">
+									<div class="nz-content-box nz-clearfix v1 fade" data-columns="1">
+										<div id="nz-box-3" class="border-active  nz-box active">
+											<div class="box-icon-wrap">
+												<div class="box-icon icon-envio-gratis"></div>
+											</div>
+											<div class="box-data">
+												<h3 class="text-center color-primary">Envío gratis</h3>
+												<p class="p1 text-center">Para artículos con precio mayor a $1,000 dentro de la República Mexicana</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="nz-section horizontal animate-false full-width-false " data-animation-speed="35000" data-parallax="false">
+					<div class="nz-row">
+						<div class="col col12  col-animate-false active" data-align="left" data-effect="fade-left" data-margin="false">
+							<div class="col-inner">
+								<div id="nz-tagline-1">
+									<a href="<?php echo site_url('/producto-categoria/relojes'); ?>" class="nz-tagline nz-clearfix">
+										<div class="container">
+											<div class="tagline-title">Checa todos los relojes que tenemos para ti</div>
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+		<!-- post end -->
+	</div>
+	<!-- content end -->
+</div>
 <?php get_footer( ); ?>
