cd12d0dc2c348ba1fcddde950969ca2b7126482c
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2017-03-30 11:49:45 -0600

    form otros productos

--- themes/unitheme-child/inc/pages.php
+++ themes/unitheme-child/inc/pages.php
@@ -89,4 +89,16 @@ add_action('init', function(){
 		wp_insert_post( $page, true );
 	}
 
+	// Otros productos
+	if( ! get_page_by_path('otros-productos') ){
+		$page = array(
+			'post_author' => 1,
+			'post_status' => 'publish',
+			'post_title'  => 'Otros productos',
+			'post_name'   => 'otros-productos',
+			'post_type'   => 'page'
+		);
+		wp_insert_post( $page, true );
+	}
+
 });
