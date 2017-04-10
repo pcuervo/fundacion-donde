61d9c7b1d3c4326f7d95973d936f1ed87c033005
Jonas Graterol <jonas@pcuervo.com>
2017-04-10 16:29:59 -0500

    Carga CSV - columna 16 no obligatoria

--- plugins/csv-importer/class-ait-import.php
+++ plugins/csv-importer/class-ait-import.php
@@ -357,7 +357,7 @@ class AitImport {
 		for($i = 0; $i<=19; $i++) {
 			//VALIDAR CAMPOS OBLIGAROTIOS
 			$col = $i+1;
-			if(in_array($i, array(0,1,2,3,4,6,7,8,9,15 ))) {
+			if(in_array($i, array(0,1,2,3,4,6,7,8,9 ))) {
 				if(!isset($data[$i]) || empty($data[$i])) {
 					$respu .= 'La columna '.$col.' es obligatoria, no debe estar vacia.<br>';
 					$ok = false;
