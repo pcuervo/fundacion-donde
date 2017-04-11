5f0aa6681315964bdeee039a678cc59aad202104
Jonas Graterol <jonas@pcuervo.com>
2017-04-11 12:01:23 -0500

    Carga CSV - Deshacer carga

--- plugins/csv-importer/class-ait-import.php
+++ plugins/csv-importer/class-ait-import.php
@@ -179,9 +179,7 @@ class AitImport {
 			$sql = "CREATE TABLE $table_name (
 					  	`bulk_load_id` int(10) unsigned DEFAULT NULL,
 						`post_id` int(10) unsigned DEFAULT NULL,
-						`mid` varchar(100) DEFAULT NULL,
-						`upc` varchar(100) DEFAULT NULL,
-						`talla` varchar(45) DEFAULT NULL,
+						`sku` varchar(100) DEFAULT NULL,
 						`cantidad` int(11) DEFAULT NULL,
 						`comentarios` varchar(500) DEFAULT NULL,
 						`status` varchar(3) DEFAULT NULL
@@ -403,6 +401,215 @@ class AitImport {
         return $san_text;
     }
 
+    function deshacer_carga($bulk_id) {
+    	global $wpdb;
+    	$actu = true;
+    	$actu_detail = true;
+    	$bulk_detail = $wpdb->get_results("SELECT bld.* FROM ".$wpdb->prefix."bulk_load_detail bld WHERE bld.status = '1' and bld.bulk_load_id = ".$_POST["bulk_id"]);
+    	foreach ($bulk_detail as $detail) {
+    		$ventas = $wpdb->get_results("SELECT p.* FROM ".$wpdb->prefix."woocommerce_order_itemmeta p WHERE meta_key = '_product_id' and meta_value = ".$detail->post_id);
+
+    		$produ = get_post($detail->post_id);
+    		$meta_existe = get_post_meta($detail->post_id);
+			$stock = $meta_existe['_stock'][0] - $detail->cantidad;
+			update_post_meta( $detail->post_id, '_stock', $stock );
+			$papa_id = wp_get_post_parent_id( $detail->post_id );
+			//echo $detail->post_id.' ventas('.count($ventas).')<br>';
+			if(count($ventas) <= 0 && $stock <= 0) {
+				
+				wp_delete_post($detail->post_id);
+				echo '<div class="error"><p><h5>PRODUCTO ELIMINADO: <strong>['.$produ->post_title.' - SKU : '.$detail->sku.']</strong></h5>'.$respues.'</p></div>';
+				//ELIMINAR EL PADRE NI NO TIENE HIJOS
+				if($papa_id) {
+					$args = array(
+						'post_parent' => $papa_id,
+						'post_type'   => 'product_variation', 
+						'numberposts' => -1,
+						'post_status' => 'publish' 
+					);
+					$children = get_children( $args );
+					//echo "hijos del padre -> ".count($children).'<br>';
+					if(count($children) <= 0) {
+						wp_delete_post($papa_id);
+					}
+				}
+			}
+			else {
+				if($stock <= 0) {
+					update_post_meta( $detail->post_id, '_stock_status', 'outofstock' );
+
+					$enstock = false;
+					if($papa_id) {
+						$args = array(
+							'post_parent' => $papa_id,
+							'post_type'   => 'product_variation', 
+							'numberposts' => -1,
+							'post_status' => 'publish' 
+						);
+						$children = get_children( $args );
+						foreach ($children as $child ) {
+							$stock_hijo = get_post_meta( $child->ID, '_stock', true );
+							if($stock_hijo > 0) {
+								$enstock = true;
+								break;
+							}
+						}
+						if(!$enstock) {
+							update_post_meta( $papa_id, '_stock_status', 'outofstock' );
+						}
+					}
+				}
+				else {
+					if(count($ventas) <= 0) {
+						echo '<div class="success"><p><h5>EL PRODUCTO NO SE PUEDE ELIMINAR PORQUE YA SE REGISTRO EN AL MENOS UNA COMPRA: <strong>['.$produ->post_title.' - SKU : '.$detail->sku.']</strong></h5>'.$respues.'</p></div>';
+						$actu_detail = false;
+						$actu = false;
+					}
+					else {
+						update_post_meta( $detail->post_id, '_stock_status', 'instock' );
+						update_post_meta( $papa_id, '_stock_status', 'instock' );
+					}
+				}
+			}
+
+			if($actu_detail) {
+				$wpdb->update(
+							$wpdb->prefix . 'bulk_load_detail',
+							array( 'status' => '0' ),
+							array( 'bulk_load_id' => $bulk_id, 'post_id' => $detail->post_id ),
+							array( '%s' ),
+							array( '%d', '%d' )
+						);
+			}
+    	}
+
+    	if($actu) {
+	    	$wpdb->update(
+							$wpdb->prefix . 'bulk_load',
+							array( 'status' => '0' ),
+							array( 'id' => $bulk_id ),
+							array( '%s' ),
+							array( '%d' )
+						);
+	    }
+
+    }
+
+    function eliminar_detalle($bulk_id, $post_id, $cant) {
+    	global $wpdb;
+    	$meta_existe = get_post_meta($post_id);
+		$stock = $meta_existe['_stock'][0] - $cant;
+		update_post_meta( $post_id, '_stock', $stock );
+
+		$wpdb->update(
+					$wpdb->prefix . 'bulk_load_detail',
+					array( 'status' => '0' ),
+					array( 'bulk_load_id' => $bulk_id, 'post_id' => $post_id ),
+					array( '%s' ),
+					array( '%d', '%d' )
+				);
+    }
+
+    function restaurar_detalle($bulk_id, $post_id, $cant) {
+    	global $wpdb;
+    	$meta_existe = get_post_meta($post_id);
+		$stock = $meta_existe['_stock'][0] + $cant;
+		update_post_meta( $post_id, '_stock', $stock );
+
+		$wpdb->update(
+					$wpdb->prefix . 'bulk_load_detail',
+					array( 'status' => '1' ),
+					array( 'bulk_load_id' => $bulk_id, 'post_id' => $post_id ),
+					array( '%s' ),
+					array( '%d', '%d' )
+				);
+    }
+
+    public function validate_headers($data) {
+		$i = 0;
+		$ok = true;
+		$respu = '';
+		if($data[$i] != 'sucursal') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "sucursal"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'estado') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "estado"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'sku') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "sku"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'nombre') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "nombre"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'precio') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "precio"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'descripcion') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "descripcion"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'cantidad') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "cantidad"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'categoria') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "categoria"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'subcategoria') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "subcategoria"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'foto1') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "foto1"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'foto2') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "foto2"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'foto3') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "foto3"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'foto4') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "foto4"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'foto5') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "foto5"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($data[$i] != 'foto6') {
+			$respu .= 'Columna '.$i.' ('.$data[$i].') debe llamarse "foto6"<br>';
+			$ok = false;
+		}
+		$i++;
+		if($ok) {
+			$respu .= "OK";
+		}
+		return $respu;
+	}
+
 	public function validate_row($data) {
 		global $wpdb;
 		$i = 0;
@@ -504,6 +711,26 @@ class AitImport {
 	}
 
 	/**
+	 * Insert an row at bulk_load_detail
+	 */
+	public function insert_row_detail($bulk_load_id, $post_id, $sku, $cantidad, $comentarios) {
+		global $wpdb;
+		$list_data = array(
+			'bulk_load_id'		=> $bulk_load_id,
+			'post_id'			=> $post_id,
+			'sku' 				=> $sku,
+			'cantidad' 			=> $cantidad,
+			'comentarios' 		=> $comentarios,
+			'status'			=> '1'
+		);
+		$wpdb->insert(
+			$wpdb->prefix . 'bulk_load_detail',
+			$list_data,
+			array( '%d', '%d', '%s', '%s', '%s', '%d', '%s', '%s' )
+		);
+	}
+
+	/**
 	 * Import items from CSV file
 	 *
 	 * @since 1.0.0
@@ -511,9 +738,12 @@ class AitImport {
 	 * @param  string $type      post type id
 	 * @param  sting $file      url to temp csv file
 	 * @param  string $duplicate how to handle duplicate items
+	 * @param  string $name 		name of the file uploaded
 	 * 
 	 */
-	public function import_csv($type, $file, $duplicate, $statusProductos, $porcentaje) {
+	public function import_csv($type, $file, $duplicate, $name, $statusProductos, $porcentaje) {
+
+		global $wpdb;
 
 		$encoding_id = intval( get_option( 'ait_import_plugin_encoding', '25' ) );
 		$encoding_list = mb_list_encodings();
@@ -552,6 +782,27 @@ class AitImport {
 					$header_line = 2;
 				}
 				if ($row == $header_line) {
+					$msj = $this->validate_headers($data_row);
+					if($msj != 'OK') {
+						echo '<div class="error"><p>Errores en las cabeceras del archivo, Por favor intente de nuevo con un archivo correcto. <a href="admin.php?page=ait-import" class="btn"> INTENTARLO DE NUEVO</a><br><strong>'.$msj.'</strong> </p></div>';
+						exit();
+					}
+
+					$bulk_load_id = 0;
+					$list_data = array(
+						'date'			=> current_time( 'mysql' ),
+						'file'			=> $name,
+						'comentarios' 	=> '',
+						'status'		=> '1',
+						'ajuste'		=> '0',
+					);
+					$wpdb->insert(
+						$wpdb->prefix . 'bulk_load',
+						$list_data,
+						array( '%s', '%s', '%s', '%s', '%s' )
+					);
+					$bulk_load_id = $wpdb->insert_id;
+
 					$cols = count($data_row);
 					for ($c=0; $c < $cols; $c++) {
 						if (in_array($data_row[$c],array_keys($post_type->default_options))){
@@ -741,6 +992,7 @@ class AitImport {
 									}
 									
 								}
+								$this->insert_row_detail($bulk_load_id, $post_id, $this->sanitize_txt($data_row[2]), $data_row[6], 'Nuevo articulo');
 								$img_galery = substr($img_galery, 0, -2);
 								update_post_meta( $post_id, '_product_image_gallery', $img_galery );
 							}
--- plugins/csv-importer/views/admin.php
+++ plugins/csv-importer/views/admin.php
@@ -26,10 +26,11 @@ $import = AitImport::get_instance();
 	}
 	// import posts from uploaded file
 	if(isset($_FILES["posts_csv"]) && isset($_POST["type"])) {
-		if ($_FILES["posts_csv"]["error"] > 0) {
-			echo '<div class="error"><p>'.__('Incorrect CSV file').'.</p></div>';
+		$ext = explode('.', $_FILES["posts_csv"]['name']);
+		if ($_FILES["posts_csv"]["error"] > 0 || $ext[count($ext)-1] != 'csv') {
+			echo '<div class="error"><p>'.__('Incorrect CSV file').'. Por favor intente de nuevo con un archivo correcto.</p></div>';
 		} else {
-			$import->import_csv($_POST["type"],$_FILES["posts_csv"]['tmp_name'],$_POST["duplicate"], $_POST["statusProductos"], $_POST["porcentaje"]);
+			$import->import_csv($_POST["type"],$_FILES["posts_csv"]['tmp_name'],$_POST["duplicate"], $_FILES["posts_csv"]['name'], $_POST["statusProductos"], $_POST["porcentaje"]);
 		}
 		
 	}
@@ -40,6 +41,20 @@ $import = AitImport::get_instance();
 		} else {
 			$import->import_terms_csv($_POST["type"],$_FILES["categories_csv"]['tmp_name'],$_POST["duplicate"]);
 		}
+	}
+	// eliminar una carga con todos sus articulos
+	if(isset($_POST["deshacer"]) && isset($_POST["bulk_id"]) && isset($_POST["deshacer_confirmar"]) ) {
+		$import->deshacer_carga($_POST["bulk_id"]);
+	}
+
+	// eliminar un registro de alguna de las cargas
+	if(isset($_POST["eliminar_detalle"]) && isset($_POST["bulk_id"]) && isset($_POST["post_id"]) && isset($_POST["cantidad"]) ) {
+		$import->eliminar_detalle($_POST["bulk_id"], $_POST["post_id"], $_POST["cantidad"]);
+	}
+
+	// eliminar un registro de alguna de las cargas
+	if(isset($_POST["restaurar_detalle"]) && isset($_POST["bulk_id"]) && isset($_POST["post_id"]) && isset($_POST["cantidad"]) ) {
+		$import->restaurar_detalle($_POST["bulk_id"], $_POST["post_id"], $_POST["cantidad"]);
 	}
 	?>
 	<!--
@@ -73,6 +88,141 @@ $import = AitImport::get_instance();
 		</div>
 	</div>
 	-->
+	<?php 
+		global $wpdb;
+		
+		if((isset($_POST["detalle"]) && isset($_POST["bulk_id"])) || (isset($_POST["eliminar_detalle"]) || isset($_POST["restaurar_detalle"]))) {
+			$bulk_detail = $wpdb->get_results("SELECT bld.* FROM ".$wpdb->prefix."bulk_load_detail bld WHERE bld.bulk_load_id = ".$_POST["bulk_id"]);
+
+			if(!empty( $bulk_detail )) {
+				echo '<div class="import-custom-type metabox-holder">';		
+				echo '<div class="import-options postbox">';
+				echo '<button type="button" class="handlediv button-link" aria-expanded="true"><span class="screen-reader-text">Alternar panel: Actividad</span><span class="toggle-indicator" aria-hidden="true"></span></button>';
+				echo '<h3 class="hndle"><span>DETALLE DE LA CARGA ID: '.$_POST["bulk_id"].'</span></h3>';
+				echo '<div class="inside">';
+				echo '<table style="width:100%;">';
+				echo '<thead>
+							<th width="30%" style="text-align:left;">Nombre</th>
+							<th width="20%" style="text-align:left;">Comentarios</th>
+							<th width="15%" style="text-align:center;">SKU</th>
+							<th width="5%" style="text-align:center;">Cantidad</th>
+							<th width="10%" style="text-align:center;">Acciones</th>
+						</thead>';
+				echo '<tbody>';
+				foreach ($bulk_detail as $detail) {
+					$produ = get_post($detail->post_id);
+					$stilo = '';
+					if($detail->status == '1') { $stilo = 'background-color:#dc3232'; }
+					if($detail->status == '0') { $stilo = 'background-color:#ffb900'; }
+					
+					echo '<tr >';
+					echo '<td>'.$produ->post_title.'</td>';
+					echo '<td>'.$detail->comentarios.'</td>';
+					echo '<td style="text-align:center;">'.$detail->sku.'</td>';
+					echo '<td style="text-align:center;">'.$detail->cantidad.'</td>';
+					echo '<td style="text-align:center;">
+							<form action="admin.php?page=ait-import" method="post">';
+								if($detail->status == '1') {  
+									echo '<input style="'.$stilo.'" class="btn button " id="btnSendToSystem" type="submit" value="ELIMINAR">';
+									echo '<input type="hidden" name="eliminar_detalle" >';
+								}
+								else {
+									echo '<input style="'.$stilo.'" class="btn button " id="btnSendToSystem" type="submit" value="RESTAURAR">';
+									echo '<input type="hidden" name="restaurar_detalle" >';
+								}
+								echo '<input type="hidden" name="bulk_id" value="'.$_POST["bulk_id"].'" >
+								<input type="hidden" name="post_id" value="'.$detail->post_id.'" >
+								<input type="hidden" name="cantidad" value="'.$detail->cantidad.'" >
+							</form>
+						</td>';
+					echo '</tr>';
+				}
+				echo '</tbody></table></div></div></div>';
+			}
+			else {
+				echo '<div class="error"><p>No se encontro ningun registro de productos para la carga con ID  '.$_POST["bulk_id"].'. <br /></div>';
+			}
+
+		}
+
+		if((isset($_POST["vercargas"])) || (isset($_POST["detalle"]) && isset($_POST["bulk_id"])) || (isset($_POST["eliminar_detalle"]) || isset($_POST["restaurar_detalle"])) || isset($_POST["deshacer"]) ){
+			$bulks = $wpdb->get_results("SELECT bl.*, (select count(bulk_load_id) from ".$wpdb->prefix."bulk_load_detail where bulk_load_id = bl.id) as registros FROM ".$wpdb->prefix."bulk_load bl WHERE bl.status = '1'");
+			
+			if(!empty( $bulks )) {
+				foreach ($bulks as $bulk) {
+					echo '<div class="error">
+						
+							<table style="width:100%;">
+								<thead>
+									<th width="5%">ID</th>
+									<th width="10%">Fecha</th>
+									<th width="40%">Archivo</th>
+									<th width="10%">Registros</th>
+									<th width="35%">Acciones</th>
+								</thead>
+								<tbody>
+									<th>'.$bulk->id.'</th>
+									<th>'.$bulk->date.'</th>
+									<th>'.$bulk->file.'</th>
+									<th>'.$bulk->registros.'</th>
+									<th>';
+									if(isset($_POST["deshacer"]) && isset($_POST["bulk_id"]) && $_POST["bulk_id"] == $bulk->id) {
+										echo '<h4>¿Estas seguro que deseas eliminar esta carga?</h4>';
+										echo '<form action="admin.php?page=ait-import" method="post">
+											<input class="btn button button-primary" style="background-color:#ffb900;" id="btnSendToSystem" type="submit" value="CONFIRMAR">
+											<input type="hidden" name="deshacer" >
+											<input type="hidden" name="deshacer_confirmar" >
+											<input type="hidden" name="bulk_id" value="'.$bulk->id.'" >
+										</form>
+										<form action="admin.php?page=ait-import" method="post">
+											<input class="btn button button-primary" id="btnSendToSystem" type="submit" value="CANCELAR">
+										</form>';
+									}
+									else {
+										if(isset($_POST["enviar"]) && isset($_POST["bulk_id"])) {
+											echo '<h4>¿Estas seguro que deseas enviar los registros de esta al SIL?</h4>';
+											echo '<form action="admin.php?page=ait-import" method="post">
+												<input class="btn button button-primary" style="background-color:#ffb900;" id="btnSendToSystem" type="submit" value="CONFIRMAR">
+												<input type="hidden" name="enviar" >
+												<input type="hidden" name="enviar_confirmar" >
+												<input type="hidden" name="bulk_id" value="'.$bulk->id.'" >
+											</form>
+											<form action="admin.php?page=ait-import" method="post">
+												<input class="btn button button-primary" id="btnSendToSystem" type="submit" value="CANCELAR">
+											</form>';
+										}
+										else {
+											echo '
+											<form action="admin.php?page=ait-import" method="post">
+												<input class="btn button button-primary" id="btnSendToSystem" type="submit" value="DETALLE DE LA CARGA">
+												<input type="hidden" name="detalle" >
+												<input type="hidden" name="bulk_id" value="'.$bulk->id.'" >
+											</form>
+											<form action="'.AIT_IMPORT_PLUGIN_URL . 'load-report.php" method="post">
+												<input class="btn button button-primary" id="btnSendToSystem" type="submit" value="REPORTE DE LA CARGA">
+												<input type="hidden" name="reporte_carga" >
+												<input type="hidden" name="bulk_id" value="'.$bulk->id.'" >
+											</form>
+											<form action="admin.php?page=ait-import" method="post">
+												<input class="btn button button-primary" id="btnSendToSystem" type="submit" value="DESHACER CARGA">
+												<input type="hidden" name="deshacer" >
+												<input type="hidden" name="bulk_id" value="'.$bulk->id.'" >
+											</form>';
+										}
+									}
+									echo '</th>
+								</tbody>
+							</table>
+							</p>
+							
+						</form>
+					</div>';
+				}
+				
+				
+			}
+		}
+	?>
 	<?php
 	foreach ($import->post_types as $type) { ?>
 	<div class="import-custom-type metabox-holder">
@@ -81,10 +231,17 @@ $import = AitImport::get_instance();
 
 			<div class="handlediv" title="Click to toggle"><br></div>
 
-			<h3 class="hndle"><span><?php echo $type->name; ?></span></h3>
+			<h3 class="hndle"><span><?php echo $type->name; ?></span>
+				<form action="admin.php?page=ait-import" method="post">
+					<input type="hidden" name="vercargas" >
+					<input class="btn button button-primary" id="btnVerCargas" type="submit" value="VER CARGAS ANTERIORES">
+				</form>
+			</h3>
 
 			<div class="inside">
-			
+				<form action="<?php echo AIT_IMPORT_PLUGIN_URL . 'load-report-undone.php'; ?>" method="post">
+					<input class="btn button button-primary" id="btnSendToSystem" type="submit" value="REPORTE DE CARGAS DESHECHAS">
+				</form>
 				<form action="<?php echo AIT_IMPORT_PLUGIN_URL . 'download.php'; ?>" method="post">
 					
 					<h4><?php _e('Importar productos masivamente desde archivos CSV'); ?></h4>
@@ -115,6 +272,7 @@ $import = AitImport::get_instance();
 					<input type="file" name="posts_csv">
 					<input type="submit" value="<?php _e('Import from CSV'); ?>" class="upload button-primary">
 				</form>
+				
 
 				
 
@@ -124,4 +282,5 @@ $import = AitImport::get_instance();
 
 	</div>
 	<?php } ?>
+	
 </div>
