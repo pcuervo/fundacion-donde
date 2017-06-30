b81b08870e62fabef2f34b248adcee11b0511518
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2017-06-30 08:21:28 -0500

    eliminada page ofertas

--- themes/unitheme-child/page-ofertas.php
+++
@@ -1,6 +0,0 @@
-<?php get_header( ); ?>
-<section class="[ container ]">
-	<h2 class="[ color-primary ]">Productos en oferta</h2>
-	<?php echo do_shortcode('[sale_products per_page="100" columns="3"]'); ?>
-</section>
-<?php get_footer( ); ?>
